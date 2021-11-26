d6f146d4dd30f75d1001268145d74c3ece323d2c
Carlos Aguirre <craguirre952@gmail.com>
2021-11-25 21:48:37 -0500

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style></style>
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+<script>
+  import axios from "axios";
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to Vue.js!",
+        cocktails: []
+      };
+    },
+    created: function () {
+      this.cocktailsIndex();
+    },
+    methods: {
+      cocktailsIndex: function () {
+        console.log("in cocktails index");
+        axios.get("http://localhost:3000/cocktails").then((response) => {
+          console.log(response.data);
+          this.cocktails = response.data;
+        });
+      }
+    },
+  };
 </script>
